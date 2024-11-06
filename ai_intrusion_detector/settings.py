f85f69d50a3333a2d60d673f8ab03f3d65a1990a
BBayramN <bayram.bakhshaliyev@gmail.com>
2024-11-07 00:05:35 +0300

    fix circle dependency docker

--- ai_intrusion_detector/settings.py
+++ ai_intrusion_detector/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a3yqkl+ikvkt+&$*%k%abo#&h)2yed*(lugz4j+gs5(ne(og4n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
