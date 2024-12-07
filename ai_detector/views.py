39ac74507324dc4e524ac3d0930d8f06dc0e4e82
BBayramN <bayram.bakhshaliyev@gmail.com>
2024-12-08 00:27:08 +0300

    settings.py wsgi fix

--- ai_detector/views.py
+++ ai_detector/views.py
@@ -10,29 +10,29 @@ def hello(request):
     return HttpResponse("SSH key test...")
 
 
-import pickle
-from django.http import JsonResponse
-from .utils import extract_model_features, preprocess_features
+# import pickle
+# from django.http import JsonResponse
+# from .utils import extract_model_features, preprocess_features
 
-# Load the trained AI model
-with open('webattack_detection_rf_model.pkl', 'rb') as model_file:
-    ai_model = pickle.load(model_file)
-@csrf_exempt
+# # Load the trained AI model
+# with open('webattack_detection_rf_model.pkl', 'rb') as model_file:
+#     ai_model = pickle.load(model_file)
+# @csrf_exempt
 def test_request(request):
-    """Test request against the trained model."""
-    # Step 1: Extract features
+#     """Test request against the trained model."""
+#     # Step 1: Extract features
     
-    if request.method == "POST":
+#     if request.method == "POST":
 
-        raw_features = extract_model_features(request)
+#         raw_features = extract_model_features(request)
 
-        # Step 2: Preprocess features
-        processed_features = preprocess_features(raw_features)
+#         # Step 2: Preprocess features
+#         processed_features = preprocess_features(raw_features)
 
-        # Step 3: Predict using the model
-        prediction = ai_model.predict([processed_features])  # Ensure input is a 2D array
+#         # Step 3: Predict using the model
+#         prediction = ai_model.predict([processed_features])  # Ensure input is a 2D array
 
-        # Step 4: Return response
-        if prediction[0] == 1:  # Assuming 1 = Attack, 0 = Normal
-            return JsonResponse({'message': 'Attack detected!'}, status=403)
+#         # Step 4: Return response
+#         if prediction[0] == 1:  # Assuming 1 = Attack, 0 = Normal
+#             return JsonResponse({'message': 'Attack detected!'}, status=403)
     return JsonResponse({'message': 'Normal request'})
--- ai_intrusion_detector/settings.py
+++ ai_intrusion_detector/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'ai_mid.AIDetectionMiddleware',
+
 ]
 
 ROOT_URLCONF = 'ai_intrusion_detector.urls'
