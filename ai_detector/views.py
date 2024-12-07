24788bd2397f66293ab8c50f780e2a604291be91
BBayramN <bayram.bakhshaliyev@gmail.com>
2024-12-07 23:34:42 +0300

    ssh key gen 2

--- ai_detector/views.py
+++ ai_detector/views.py
@@ -7,7 +7,7 @@ def hello(request):
     if request.method == "POST":
         pp = pprint.PrettyPrinter(indent=4)
         pp.pprint(request.META)
-    return HttpResponse("Hello, worldaaaaaa222...")
+    return HttpResponse("Hello...")
 
 
 import pickle
