3e4870e722a42aac952102f76b62119c01a1730f
BBayramN <bayram.bakhshaliyev@gmail.com>
2024-12-09 19:51:14 +0300

    views update -utils.py 2

--- ai_detector/urls.py
+++ ai_detector/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home,name='home'),
     path('capture-traffic/', views.capture_traffic, name='capture_traffic'),
 ]
